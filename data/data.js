b3de511b2ce1de132002fb2dd0dab9cfcba9055b
Daniel Esteban Martinez Herrera <dan.ielestebanmat@gmail.com>
2024-09-12 19:57:28 -0500

    .

---
+++ data/data.js
@@ -0,0 +1,27 @@
+export const data = [
+  {
+    question: "Quién fue el primer entrenador en ganar la Champions League con tres equipos diferentes?",
+    choices: ["Pep Guardiola", "José Mourinho", "Diego Simeone", "Carlo Ancelotti"],
+    answer: "José Mourinho",
+  },
+  {
+    question: "En qué país se jugó el primer Mundial de Fútbol",
+    choices: ["Brasil", "Argentina", "Uruguay", "Alemania"],
+    answer: "Uruguay",
+  },
+  {
+    question: "Cuál es la única selección que ha participado en todos los Mundiales?",
+    choices: ["Brasil", "Italia", 'Portugal', 'Inglaterra'],
+    answer: "Brasil",
+  },
+  {
+    question: "Quién es el máximo goleador histórico de la Champions League?",
+    choices: ["Maradona", "Cristiano Ronaldo", "Falcao", 'James Rodriguez', 'Messi'],
+    answer: "Cristiano Ronaldo",
+  },
+  {
+    question: "Cuál fue el jugador más joven en ganar una Copa del Mundo?",
+    choices: ["Pelé ", "Kylian Mbappé", "Ronaldo Nazário", 'Messi'],
+    answer: "Pelé ",
+  },
+];
